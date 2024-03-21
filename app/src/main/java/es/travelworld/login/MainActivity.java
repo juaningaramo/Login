ecad74053c76c3f5c5a6441bbe30ff1151f20ee3
juaningaramo <juani.river31@gmail.com>
2024-03-21 11:08:28 +0100

    Limpiar campos cuando clickeen boton

--- app/src/main/java/es/travelworld/login/MainActivity.java
+++ app/src/main/java/es/travelworld/login/MainActivity.java
@@ -3,30 +3,40 @@ package es.travelworld.login;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.ViewDataBinding;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import es.travelworld.login.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
-        // buscamos la vista a clickar
-        TextView textView = findViewById(R.id.tv_registrar);
-        TextView textView2 = findViewById(R.id.tv_recuperar);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
 
         // inicializamos la snackbar con la info a mostrar
         Snackbar snackbar = Snackbar.make(findViewById(R.id.container).getRootView(), "Proximamente...", Snackbar.LENGTH_SHORT);
         Snackbar snackbar2 = Snackbar.make(findViewById(R.id.container).getRootView(), "Proximamente...", Snackbar.LENGTH_SHORT);
 
         // le decimos a nuestra vista que cuando la clicken, muestre el snackbar
-        textView.setOnClickListener(v -> snackbar.show());
-        textView2.setOnClickListener(v -> snackbar.show());
+        binding.tietUsuario.setOnClickListener(v -> snackbar.show());
+        binding.tietContrasena.setOnClickListener(v -> snackbar.show());
 
-
+        binding.bLogin.setOnClickListener(v -> {
+            binding.tietUsuario.setText("");
+            binding.tietContrasena.setText("");
+            binding.tietContrasena.clearFocus();
+        });
     }
 
 }
---
+++ app/src/main/java/es/travelworld/login/Usuario.java
@@ -0,0 +1,28 @@
+package es.travelworld.login;
+
+public class Usuario {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Usuario(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Usuario() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                '}';
+    }
+}
